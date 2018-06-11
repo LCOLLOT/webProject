58a29a28cd720c6691ca818586dc7651abb02463
SelenEris <40161882+SelenEris@users.noreply.github.com>
2018-06-12 00:03:00 +0200

    changement du bouton de validation dans "contact"

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@ include ('affichage/header.php');
                     <tr><td>Adresse mail <input type="email" name="email" class="form-control"/></td></tr>
                     <tr><td>Sujet <input type="text" name="sujet" class="form-control"/></td></tr>
                     <tr><td>Message  <textarea name="message" rows="15" cols="80"></textarea></td></tr>
-                    <a class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-pencil"></span> Valider</a>
+                    <tr><td><button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-send"></span> Envoyer</button></td></tr>
                 </form>
             </table>
         </div>
