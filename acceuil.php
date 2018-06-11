48a0d25c3754a8d95593eced23d16873de612ac2
Lucas COLLOT <lucascp@hotmail.fr>
2018-06-11 17:57:20 +0200

    ajout de la barre de recherche sur l'acceuil

--- acceuil.php
+++ acceuil.php
@@ -18,21 +18,47 @@
 </div>
 
 <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12" id="searchBarre" align="center">
+
     <div class="col-lg-3 col-md-3 col-sm-3">
-    <!-- Menu à insérer -->
-
-        <form class="well" method="post" action="acceuil.php">
-
+        <form method="post" action="acceuil.php">
             <table align="center">
                 <tbody>
-                <tr><td class="titreForm">Recherche</td></tr>
+                <tr><td>Recherche</td></tr>
                 <tr><td><input type="text" name="recherche" class="form-control"/></td></tr>
-                <tr><td><button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-ok-sign"></span>Rechercher</button></td></tr>
+                <tr><td align="center"><button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-ok-sign"></span>Rechercher</button></td></tr>
                 </tbody>
             </table>
         </form>
     </div>
-    <div class="col-lg-9 col-md-9 col-sm-9">
+        <div class="col-lg-3 col-md-3 col-sm-3">
+            <form method="post" action="acceuil.php">
+                <table align="center">
+                    <tbody>
+                    <tr><td>Autour de moi</td></tr>
+                    <tr><td><button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-ok-sign"></span>Rechercher</button></td></tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <form method="post" action="acceuil.php">
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>Longitude <input type="text" name="longitude" class="form-control"/></td>
+                        <td>Lattitude <input type="text" name="lattitude" class="form-control"/></td>
+                    </tr>
+                    <tr><td><button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-ok-sign"></span>Rechercher</button></td></tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <br>
+    <div class="col-lg-offset-2 col-md-offset-2  col-sm-offset-2  col-lg-10 col-md-10 col-sm-10">
             <!-- Affichage des articles correspondant à la requete dans la barre de recherche-->
             <?php
             if(isset($_POST['recherche']) && !empty($_POST['recherche'])) {
