55f11a6d14b32d79d30951df3e11d748ea714a14
SamanthaLaiyoeurn <naty_coul@hotmail.fr>
2018-06-11 17:06:05 +0200

    j'ai rajouté un e à Bienvenu

---
+++ mentionsLegales.php
@@ -0,0 +1,42 @@
+<?php
+include ('affichage/header.php');
+?>
+
+    <div class="entry-content">
+        <p>Le site www.web-trotter.fr est une publication de la société WEB-TROTTER.</p>
+        <p>WEB-TROTTER est une S.A.R.L au capital de 0 euros.</p>
+        <p>Siège social : 12 Rue des Frères Lumière,  68093 Mulhouse – contact[at]webtrotter.fr</p>
+        <p>Directeur de la publication : Monsieur Collot-Penichot Lucas</p>
+        <p>La société WEB-TROTTER est soumise au droit français.</p>
+        <p>Hébergeur : Ce site web est hébergé par OVH, SAS au capital de 10 069 020 €
+            <br>Siège social : 2 rue Kellermann - 59100 Roubaix - France</p>
+
+        <h3>Droit des marques :</h3>
+        <p>Les différentes marques citées sur ce site appartiennent aux organismes qui les ont
+            déposées et ne sont mentionées qu'à titre signalétique.</p>
+
+        <h3>Conditions d'utilisation :</h3>
+        <p>
+            Le site et chacun des éléments, y compris mais sans limitation les marques, les logos, icônes, infographies,
+            photographies, qui le composent sont protégés au titre de la législation internationale de la propriété
+            intellectuelle, conformément aux dispositions de l'article L.122-5 du Code de la Propriété Intellectuelle.
+            <br>Les logos et autres représentations graphiques de ce site sont protégés au titre des droits d'auteur et
+            des articles L.511.1 et suivants du Code de la Propriété Intellectuelle relatifs à la protection des modèles
+            déposés.
+            Sauf autorisation, toute utilisation des oeuvres autres que la reproduction et la consultation individuelles et privées sont interdites.
+            <br>Toute utilisation, reproduction ou représentation,
+            par quelque procédé que ce soit, et sur quelque support que ce soit, de tout ou partie du site et/ou des éléments qui le composent n'est pas autorisée sans le consentement expresse de WEB-TROTTEUR.
+        </p>
+
+        <h3>Responsabilité éditoriale : </h3>
+        <p>Le propriétaire du site ne saurait être tenu pour responsable des potentielles erreurs
+            dans les documents présents sur le site, malgré toute la vigilance apportée à
+            leur publication.
+            <br> Le propriétaire du site ne saurait non plus être tenu pour responsable des informations,
+            propos et opinions émis sur les sites vers lesquels il pointe par des liens hypertexte et dont il n’a pas la maîtrise éditoriale.
+        </p>
+    </div>
+
+<?php
+include ('affichage/footer.php');
+?>
