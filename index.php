4d8ec89d1f037abf87da5af4b794f9da298d20fe
Lucas COLLOT <lucascp@hotmail.fr>
2018-06-11 12:44:44 +0200

    ajout de la redirection vers la page d'acceuil après identifiaction

--- index.php
+++ index.php
@@ -27,6 +27,18 @@
         </form>
     </div>
 </div>
+<div class="row">
+    <div class="col-lg-offset-4 col-md-offset-4 col-sm-offset-4 col-lg-4 col-md-4 col-sm-4" <?php if(!$user == 'badLog') echo "hidden";?>>
+        <div id="ok">
+            <div class="panel panel-danger">
+                <div class="panel-heading" align="center">
+                    <h3 class="panel-title"><strong>Combinaison identifiant / mot de passe incorrect !
+                            <button class="btn btn-danger"><span class="glyphicon glyphicon-remove"><script>addEventListener('click', function(e) { document.getElementById('ok').style.display = 'none';}, false); </script></span></button> </strong></h3> </div>
+            </div>
+        </div>
+
+    </div>
+</div>
 
 <?php
     include ('affichage/footer.php');
