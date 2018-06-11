75493b1042c65779d32e9b667556189530a8446b
karlpalisse <karl.palisse@gmail.com>
2018-06-11 23:34:41 +0200

    barre de navigation + menu déroulant

--- messagerie.php
+++ messagerie.php
@@ -1,10 +1,20 @@
 <?php
 include ('affichage/header.php');
+include('log/pdo.php');
+$reponse = $bdd->query('SELECT users.name, messages.contenu, messages.date FROM users, messages WHERE messages.auteur_id = users.id AND destinataire_id = "'.$_SESSION['user_id'].'" ORDER BY messages.id DESC');
 ?>
 
     <h2>Messagerie de  monsieur <strong><?php echo $_SESSION['user']; ?></strong></h2>
-
-
+    <a class="btn btn-default" href="newMessage.php" role="button"><span class="glyphicon glyphicon-pencil"></span> Nouveau Message</a>
+    <!--$insertion = $bdd->prepare('INSERT INTO messages VALUES(NULL,"'.$contenu.'""'.$_SESSION['user_id'].'",);
+    $insertion->execute(); -->
+<?php
+    echo '<h3>Vos Messages :</h3>';
+    while($donnees = $reponse->fetch()){
+        echo '<h4>'.'De : '.$donnees['name'].', '.$donnees['date'].' :'.'</h4>';
+        echo '<h4>'.$donnees['contenu'].'</h4>';
+    }
+?>
 <?php
 include ('affichage/footer.php');
 ?>
