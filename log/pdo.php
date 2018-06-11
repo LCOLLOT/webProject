83703b0debd7d5c67b49ef2921d511cc241983d4
Baptiste <baptiste.st-arnoult@outlook.fr>
2018-06-11 10:43:04 +0200

    Connection à la bdd

--- log/pdo.php
+++ log/pdo.php
@@ -1,1 +1,9 @@
 <?php
+    $nomBdd='';
+    try{
+        $bdd = new PDO('mysql:host=localhost;dbname='.$nomBdd.';charset=utf8', 'root', '');
+    }
+    catch(Exception $e){
+        die('Erreur : '.$e->getMessage()); // on arrête tous les processus et on affiche le message d'erreur
+    }
+?>
