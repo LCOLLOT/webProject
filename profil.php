76c71140345ce297e27ad5525dbb90e6a9ad5d86
Lucas COLLOT <lucascp@hotmail.fr>
2018-06-11 17:37:43 +0200

    Ajout de la page profil

---
+++ profil.php
@@ -0,0 +1,19 @@
+<?php
+    include ("affichage/header.php");
+    $profil = new Profil($_SESSION['user_id']);
+?>
+<h2>Votre profil</h2>
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4">
+                    <table class="table table-bordered">
+                            <tr><td>Nom  :<?php echo $profil->getName();?></td></tr>
+                            <tr><td><img src="images/users/<?php echo $profil->getPhoto();?>" alt="<?php echo $profil->getName();?>" class="img-responsive"></td></tr>
+                            <tr><td>Date de naissance :<?php echo $profil->getDate();?></td></tr>
+                            <tr><td>Pseudonyme :<?php echo $profil->getPseudo();?></td></tr>
+                            <tr><td>Adresse mail :<?php echo $profil->getMail();?></td></tr>
+                    </table>
+    </div>
+</div>
+<?php
+include ("affichage/footer.php");
+?>
