1edd4d8c20a38ddc843e7966ad1406b3ddec2fd8
Sam <naty_coul@hotmail.fr>
2018-06-11 14:05:46 +0200

    Titre de niveau 1

--- acceuil.php
+++ acceuil.php
@@ -4,7 +4,7 @@
 
     <h2>Bienvenu monsieur <strong><?php echo $_SESSION['user']; ?></strong></h2>
 
-    <h1>Bienvenue avec un E</h1>
+    <h1>TITRE DE NIVAEU 1</h1>
 
 <?php
 include ('affichage/footer.php');
