f022f819ba2eb761216c48670696b770331d112e
SelenEris <40161882+SelenEris@users.noreply.github.com>
2018-06-13 14:24:03 +0200

    test couleurs

--- affichage/footer.php
+++ affichage/footer.php
@@ -2,12 +2,10 @@
 
     <hr>
         <!-- pied de page -->
-        <div class="fond">
         <p><a href="contact.php">Nous contacter</a></p>
         <p>2018. Créé par le groupe Web-Trotter.</p>
         <p>Membres : Lucas CP, Léa B, Samantha L, Florian C, Hamid B, Karl P, Baptiste SA</p>
         <p><a href="mentionsLegales.php">Mentions légales, Conditions d'utilisation</a></p>
-        </div>
     </footer>
 
 
