01862af4240164c84693eb040257c267861e2bbd
SelenEris <40161882+SelenEris@users.noreply.github.com>
2018-06-13 08:36:47 +0200

    ajout de l'en-tête dans la page contact (elle n'y était plus)

--- contact.php
+++ contact.php
@@ -4,27 +4,6 @@ session_start();
 session_cache_limiter('private_no_expire, must-revalidate');
 
 ?>
-    <!doctype html>
-    <html lang="fr">
-
-    <head>
-        <meta charset="utf-8" />
-        <title>Web-Trotter</title>
-        <link rel="stylesheet" href="add.css" />
-        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-    </head>
-
-    <body>
-    <!-- COMMENTAIRE : ici, je place le corps de mon site -->
-    <div class="container">
-        <nav class="navbar navbar-default">
-            <ul class="nav navbar-nav">
-                <li><a href="acceuil.php"><img src="images/logo.png" alt="web-trotter" class="logo"></a></li>
-            </ul>
-        </nav>
-        <script src="bootstrap/js/jquery.js"></script>
-        <script src="bootstrap/js/bootstrap.min.js"></script>
-
     <h2>Nous contacter</h2>
     <div class="row">
         <div class="col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-lg-5 col-md-5 col-sm-5">
