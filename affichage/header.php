ee4c8023390da483e24988cea40d7dcf3262418b
SelenEris <40161882+SelenEris@users.noreply.github.com>
2018-06-13 08:53:16 +0200

    test couleurs

--- affichage/header.php
+++ affichage/header.php
@@ -23,10 +23,13 @@ if (!isset($_SESSION['user']))
 <body>
 <!-- COMMENTAIRE : ici, je place le corps de mon site -->
 <div class="container">
+    <div class="logodiv">
+        <a href="acceuil.php"><img src="images/logo.png" alt="web-trotter" class="logo"></a>
+    </div>
         <nav class="navbar navbar-default">
             <ul class="nav navbar-nav">
-                <li><a href="acceuil.php"><img src="images/logo.png" alt="web-trotter" class="logo"></a></li>
-                    <li> <a href="acceuil.php">Accueil</a></li>
+                <li></li>
+                    <li> <a href="acceuil.php">Accueil</a> </li>
                     <li> <a href="newArticle.php">Ajouter un article</a> </li>
                     <li> <a href="messagerie.php">Messagerie</a> </li>
                     <li> <a href="contact.php">Nous contacter</a> </li>
