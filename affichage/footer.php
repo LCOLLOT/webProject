76c903ad7cc114115d9f98152871a9275b45adfb
SamanthaLaiyoeurn <naty_coul@hotmail.fr>
2018-06-14 18:38:41 +0200

    saut de ligne entre "nous contacter" et "2018"

--- affichage/footer.php
+++ affichage/footer.php
@@ -1,8 +1,9 @@
     <footer>
         <!-- pied de page -->
-             <p><a href="../contact.php">Nous contacter</a> 2018. Créé par le groupe Web-Trotter.</p>
-             <p>Membres : Lucas CP, Léa B, Samantha L, Florian C, Hamid B, Karl P, Baptiste SA</p>
-             <p><a href="../mentionsLegales.php">Mentions légales, Conditions d'utilisation</a></p>
+        <p><a href="../contact.php">Nous contacter</a>
+        <p>2018. Créé par le groupe Web-Trotter.
+        <p>Membres : Lucas CP, Léa B, Samantha L, Florian C, Hamid B, Karl P, Baptiste SA
+        <p><a href="../mentionsLegales.php">Mentions légales, Conditions d'utilisation</a>
     </footer>
 
     </div>
